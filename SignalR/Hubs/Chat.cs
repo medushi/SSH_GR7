80cbb71230bc1716ce0aeb6f27df288de16e423f
Muhamed Zeqiri <34819956+Muhamedii@users.noreply.github.com>
2020-05-30 00:53:01 +0200

    simple chat impl using SignalR

---
+++ SignalR/Hubs/Chat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace SistemeTeShperndaraGR7.SignalR.Hubs
+{
+    public class Chat: Microsoft.AspNet.SignalR.Hub
+    {
+        public void Send(string name,string message)
+        {
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+[assembly: OwinStartupAttribute(typeof(SistemeTeShperndaraGR7.Startup))]
+namespace SistemeTeShperndaraGR7
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
