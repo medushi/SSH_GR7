648956d5cad470ed834ae96324a0a3bc3789de35
Muhamed Zeqiri <34819956+Muhamedii@users.noreply.github.com>
2020-06-05 02:58:55 +0200

    structure and setup of api-s

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SistemeTeShperndaraGR7.App_Start
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration configuration)
+        {
+            configuration.MapHttpAttributeRoutes();
+            configuration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+        }
+    }
+}
